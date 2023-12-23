eb24c7a40cdcc66f7844b26a7b69545d50b634e3
Daniel Golonka <danielgolonka@Daniels-Air.lan>
2023-12-22 22:15:13 -0600

    Started model

---
+++ SolarSystemModel/model.py
@@ -0,0 +1,15 @@
+import time
+import vtkmodules.all as vtk
+
+import vtkmodules.vtkInteractionStyle
+
+import vtkmodules.vtkRenderingOpenGL2
+from vtkmodules.vtkCommonColor import vtkNamedColors
+from vtkmodules.vtkFiltersSources import vtkConeSource
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderWindow,
+    vtkRenderer
+)
+
